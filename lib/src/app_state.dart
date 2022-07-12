0032bddc40cf9adcc061d3bdca0879c5ae6669c5
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-07-12 14:43:57 +0200

    replace flutter dev channel with user input one

--- lib/src/app_state.dart
+++ lib/src/app_state.dart
@@ -4,17 +4,9 @@ import 'package:flutter/foundation.dart';
 import 'package:googleapis/youtube/v3.dart';
 import 'package:http/http.dart' as http;
 
-class FlutterDevPlaylists extends ChangeNotifier {
-  FlutterDevPlaylists({
-    required String flutterDevAccountId,
-    required String youTubeApiKey,
-  }) : _flutterDevAccountId = flutterDevAccountId {
-    _api = YouTubeApi(
-      _ApiKeyClient(
-        client: http.Client(),
-        key: youTubeApiKey,
-      ),
-    );
+class AuthedUserPlaylists extends ChangeNotifier {
+  set authClient(http.Client client) {
+    _api = YouTubeApi(client);
     _loadPlaylists();
   }
 
@@ -23,9 +15,9 @@ class FlutterDevPlaylists extends ChangeNotifier {
     _playlists.clear();
 
     do {
-      final response = await _api.playlists.list(
+      final response = await _api!.playlists.list(
         ['snippet', 'contentDetails', 'id'],
-        channelId: _flutterDevAccountId,
+        mine: true,
         maxResults: 50,
         pageToken: nextPageToken,
       );
@@ -37,8 +29,7 @@ class FlutterDevPlaylists extends ChangeNotifier {
     } while (nextPageToken != null);
   }
 
-  final String _flutterDevAccountId;
-  late final YouTubeApi _api;
+  YouTubeApi? _api;
 
   final List<Playlist> _playlists = [];
   List<Playlist> get playlists => UnmodifiableListView(_playlists);
@@ -55,7 +46,7 @@ class FlutterDevPlaylists extends ChangeNotifier {
   Future<void> _retrievePlaylist(String playlistId) async {
     String? nextPageToken;
     do {
-      var response = await _api.playlistItems.list(
+      var response = await _api!.playlistItems.list(
         ['snippet', 'contentDetails'],
         playlistId: playlistId,
         maxResults: 25,
@@ -68,22 +59,5 @@ class FlutterDevPlaylists extends ChangeNotifier {
       notifyListeners();
       nextPageToken = response.nextPageToken;
     } while (nextPageToken != null);
-  }
-}
-
-class _ApiKeyClient extends http.BaseClient {
-  _ApiKeyClient({required this.key, required this.client});
-
-  final String key;
-  final http.Client client;
-
-  @override
-  Future<http.StreamedResponse> send(http.BaseRequest request) {
-    final url = request.url.replace(queryParameters: <String, List<String>>{
-      ...request.url.queryParametersAll,
-      'key': [key]
-    });
-
-    return client.send(http.Request(request.method, url));
   }
 }
